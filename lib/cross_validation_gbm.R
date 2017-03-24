5db7a90b7297f075b28fbb011f6cac2972f141ee
MarieSlz <marie.c.schiltz@gmail.com>
2017-03-23 21:43:22 -0400

    Last Update Gbm

--- lib/cross_validation_gbm.R
+++ lib/cross_validation_gbm.R
@@ -6,8 +6,25 @@
 ### Project 3
 ### ADS Spring 2016
 
+cv.function <- function(df.train, K, par_best){
+  n <- nrow(df.train)
+  n.fold <- floor(n/K)
+  s <- sample(rep(1:K, c(rep(n.fold, K-1), n-(K-1)*n.fold)))  
+  cv.error <- rep(NA, K)
+  
+  for (i in 2:K){
+    train.data <- df.train[s != i,]
+    test.data <- df.train[s == i,]
+    
+    fit <- train(train.data, par_best)
+    pred <- test(fit, test.data)  
+    cv.error[i] <- mean(pred != test.data$labels)  
+  }			
+  return(c(mean(cv.error),sd(cv.error)))
+  
+}
 
-cv.function <- function(train, shr, K){
+cv.function.shr <- function(train, shr, K){
   
   library(gbm)
   
